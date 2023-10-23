a2b32abdd8a1bb274e8592f8b4ffe0d66838ec80
Ruan <lopes.ruan@gmail.com>
2023-10-23 18:00:44 -0300

    seo changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @page_title = "Página Oficial"
+    @page_description = "Página oficial da banda Capivaras do Vale, banda de rock e pop rock originada na região serrana do estado do Rio de Janeiro, que tem como principal influência o rock nacional e internacional dos anos 80 e 90."
+    @page_keywords = "banda, rock, capivaras, vale, música"
   end
 end
