d75bf16c03a64af0ef9095fe9cf65d66808e532d
Ruan <lopes.ruan@gmail.com>
2023-10-23 11:35:32 -0300

    smtp config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "capivaras_rock_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => Rails.application.credentials[:smtp][:username],
+    :password => Rails.application.credentials[:smtp][:password],
+    :address => 'smtp.gmail.com',
+    :domain => 'smtp.gmail.com',
+    :port => '587',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+  config.hosts << "capivaras-lp.fly.dev"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
